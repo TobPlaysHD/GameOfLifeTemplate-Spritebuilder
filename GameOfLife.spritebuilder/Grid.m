fd1aa143000b05092aa4b97350ae0cfbce55dd5a
FlyBee <tobplayshd@gmail.com>
2015-07-13 15:38:19 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -160,15 +160,18 @@ static const int GRID_COLUMNS = 10;
             
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = TRUE;
-                numAlive += 1;
             }
             
             if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = FALSE;
             }
+            
+            if(currentCreature.isAlive == TRUE) {
+                numAlive = numAlive + 1;
+            }
         }
     }
-    _totalAlive = numAlive + 1;
+    _totalAlive = numAlive;
 }
 
 @end
